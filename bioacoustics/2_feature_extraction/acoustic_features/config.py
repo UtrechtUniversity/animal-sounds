e2f0c07c417789524fa1a9bad5e77947401e62a1
Jelle Treep <h.j.treep@uu.nl>
2022-04-13 13:44:59 +0200

    rename folder

---
+++ bioacoustics/2_feature_extraction/acoustic_features/config.py
@@ -0,0 +1,25 @@
+# -*-coding:Utf-8 -*
+import json
+
+
+class Config:
+    """
+    Object containing configuration information
+    """
+
+    def __init__(self, path):
+        """
+        Initialization method
+        """
+        self.path = path
+        self.domain = None
+        self.features = None
+
+    def read(self):
+        """
+        Read configuration file stored in self.path.
+        """
+        # Read
+        conf = json.load(open(self.path, 'rb'))
+        self.domain = conf['computation_domains']
+        self.features = conf['features']
