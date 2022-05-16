38648b945cd517b0d627c210ee55de105d56996b
Jelle Treep <h.j.treep@uu.nl>
2022-05-16 14:53:20 +0200

    add docstrings

--- bioacoustics/1_wav_processing/raven_to_wav.py
+++ bioacoustics/1_wav_processing/raven_to_wav.py
@@ -77,11 +77,37 @@ class ProcessRaven:
         self.wav_path = wav_path
 
     def raven_input(self):
-        self.df = pd.read_table(self.file)
+        """Read Raven annotations .txt file.
+
+        Reads Raven annotations file as pandas dataframe and removes uppercase characters in column names
+
+        Returns
+        -------
+        DataFrame
+            Raven annotations in dataframe format
+        """
+        if self.file[-3:] == 'csv':
+            self.df = pd.read_csv(self.file)
+        else:
+            self.df = pd.read_table(self.file)
         self.df.columns = self.df.columns.str.lower()
         print(self.df.columns)
 
-    def wav_file_len(self, wav_path):
+    def compute_file_lengths(self, wav_path):
+        """Compute file lenghts of .WAV files.
+
+        Lists .wav files from a specific folder, gets the length of the files in seconds and combines them in a dataframe
+
+        Parameters
+        ----------
+        wav_path : str
+            Location of .wav files
+
+        Returns
+        -------
+        DataFrame
+            Filenames and file lenghts in seconds
+        """
         len_file_list = []
         for f in self.filelist:
             len_file = librosa.get_duration(filename=wav_path + f)
@@ -92,51 +118,103 @@ class ProcessRaven:
         self.file_lengths = df
 
     @staticmethod
-    def fix_multifile(df_out, filelist, wav_path):
-        row_i = list(df_out[(df_out['begin_path']
-                             != df_out['end_path'])].index)
+    def split_multifile_annotations(df, filelist, wav_path):
+        """Split annotations that are composed of multiple .wav files.
+
+        Annotations that are composed on multiple .wav files are split into
+        multiple annotations that will replace the original annotation in
+        the returned datafrome.
+
+        The first part creates two dataframe rows for the .wav file where
+        the annotation starts and ends. The second part adds extra rows
+        for annotations that are composed of more than 2 .wav files.
+
+        Parameters
+        ----------
+        df : DataFrame
+            dataframe containing annotated segments
+
+        filelist : list of str
+            list containing all .wav filenames of annotated dataset.
+
+        wav_path : str
+            location of the .wav files
+
+
+        Returns
+        -------
+        DataFrame
+            Cleaned dataframe
+        """
+        row_i = list(df[(df['begin_path']
+                         != df['end_path'])].index)
 
         for i in row_i:
-            df = pd.DataFrame([[df_out.loc[i, 'begin_path'],
-                                df_out.loc[i, 'begin_path'],
-                                df_out.loc[i, 'start_time'],
-                                df_out.loc[i, 'filelength'],
-                                df_out.loc[i, 'filelength']],
-                               [df_out.loc[i, 'end_path'],
-                                df_out.loc[i, 'end_path'],
-                                0.0,
-                                df_out.loc[i, 'end_time']
-                                - df_out.loc[i, 'filelength'],
-                                df_out.loc[i, 'filelength']]],
-                              columns=['begin_path',
-                                       'end_path',
-                                       'start_time',
-                                       'end_time',
-                                       'filelength'])
+            df_temp = pd.DataFrame([[df.loc[i, 'begin_path'],
+                                     df.loc[i, 'begin_path'],
+                                     df.loc[i, 'start_time'],
+                                     df.loc[i, 'filelength'],
+                                     df.loc[i, 'filelength']],
+                                    [df.loc[i, 'end_path'],
+                                     df.loc[i, 'end_path'],
+                                     0.0,
+                                     df.loc[i, 'end_time']
+                                     - df.loc[i, 'filelength'],
+                                     df.loc[i, 'filelength']]],
+                                   columns=['begin_path',
+                                            'end_path',
+                                            'start_time',
+                                            'end_time',
+                                            'filelength'])
 
-            df_out = df_out.append(df, ignore_index=True)
+            df = df.append(df_temp, ignore_index=True)
 
-            if (filelist.index(df_out.loc[i, 'end_path'])
-                    - filelist.index(df_out.loc[i, 'begin_path'])) > 1:
-                for j in range(filelist.index(df_out.loc[i, 'begin_path'])
-                               + 1, filelist.index(df_out.loc[i, 'end_path'])):
+            if (filelist.index(df.loc[i, 'end_path'])
+                - filelist.index(df.loc[i, 'begin_path'])) > 1:
+                # In case of annotations composed of more tthis part
+                for j in range(filelist.index(df.loc[i, 'begin_path'])
+                               + 1, filelist.index(df.loc[i, 'end_path'])):
                     print(j)
                     len_file = librosa.get_duration(
                         filename=wav_path + filelist[j])
-                    df = pd.DataFrame([[filelist[j],
-                                        filelist[j],
-                                        0.0,
-                                        len_file,
-                                        len_file]],
-                                      columns=['begin_path',
-                                               'end_path',
-                                               'start_time',
-                                               'end_time',
-                                               'filelength'])
-                df_out = df_out.append(df, ignore_index=True)
-        return df_out.drop(row_i)
+                    df_temp = pd.DataFrame([[filelist[j],
+                                             filelist[j],
+                                             0.0,
+                                             len_file,
+                                             len_file]],
+                                           columns=['begin_path',
+                                                    'end_path',
+                                                    'start_time',
+                                                    'end_time',
+                                                    'filelength'])
+                df = df.append(df_temp, ignore_index=True)
+        return df.drop(row_i)
 
     def process_raven(self, species, min_sig_len):
+        """Short description.
+
+        Extended description of the function, providing a more extensive
+        description than a single-line summary.
+
+        Parameters
+        ----------
+        param1 : int
+            description
+
+            This description can span multiple lines and/or paragraphs as
+            well.
+        param2 : str, optional
+            another description
+        *args
+            Variable length argument list.
+        **kwargs
+            Arbitrary keyword arguments.
+
+        Returns
+        -------
+        bool
+            True if successful, False otherwise.
+        """
         df = self.df.rename(columns={'begin path': 'begin_path',
                                      'end path': 'end_path'})
 
@@ -168,7 +246,7 @@ class ProcessRaven:
         df_3['end_time'] = (df_3['file offset (s)']
                             + df_3['end time (s)'] - df_3['begin time (s)'])
 
-        df_3 = self.fix_multifile(df_3, self.filelist, self.wav_path)
+        df_3 = self.split_multifile_annotations(df_3, self.filelist, self.wav_path)
 
         # Select columns of interest
         df_4 = df_3.loc[:, ['begin_path',
@@ -183,8 +261,33 @@ class ProcessRaven:
             self.df_2[self.df_2['end_time']
                       - self.df_2['start_time'] < 0.2].index)
         self.duration = self.df_2['end_time'] - self.df_2['start_time']
+        self.df_2.rename(columns={"begin_path": "file"})
 
     def padding(self, df, wav_path, min_bg_padding, min_length):
+        """Short description.
+
+        Extended description of the function, providing a more extensive
+        description than a single-line summary.
+
+        Parameters
+        ----------
+        param1 : int
+            description
+
+            This description can span multiple lines and/or paragraphs as
+            well.
+        param2 : str, optional
+            another description
+        *args
+            Variable length argument list.
+        **kwargs
+            Arbitrary keyword arguments.
+
+        Returns
+        -------
+        bool
+            True if successful, False otherwise.
+        """
         self.df_pad = pd.DataFrame({'file': [], 'start': [], 'duration': []})
         for index, row in df.iterrows():
             file = wav_path + row.begin_path
@@ -206,11 +309,40 @@ class ProcessRaven:
                                               'duration': duration},
                                              ignore_index=True)
         self.df_pad['type'] = list(df['type'])
-        self.df_pad['filename'] = list(df['begin_path'])
+        self.df_pad['filename'] = list(df['file'])
 
 
-def write_files(df, output_path, species, rec_id, createframes,
-                k_start, frame_len, jump_len):
+def write_files(df, output_path,
+                species,
+                rec_id,
+                createframes,
+                k_start,
+                frame_len,
+                jump_len):
+    """Short description.
+
+    Extended description of the function, providing a more extensive
+    description than a single-line summary.
+
+    Parameters
+    ----------
+    param1 : int
+        description
+
+        This description can span multiple lines and/or paragraphs as
+        well.
+    param2 : str, optional
+        another description
+    *args
+        Variable length argument list.
+    **kwargs
+        Arbitrary keyword arguments.
+
+    Returns
+    -------
+    bool
+        True if successful, False otherwise.
+    """
     k = k_start
     kk = 0  # second fileindex for output files (when n raven files > 1)
     df2 = pd.DataFrame(columns=['id', 'recorder_id',
@@ -277,7 +409,7 @@ def main():
 
     p1 = ProcessRaven(args.raven_file, args.wavpath)
     p1.raven_input()
-    p1.wav_file_len(args.wavpath)
+    p1.compute_file_lengths(args.wavpath)
     p1.process_raven(args.species, args.min_sig_len)
     p1.padding(p1.df_2, args.wavpath, args.bg_padding_len, min_length=0.5)
     write_files(p1.df_pad, args.outputdir, args.species, args.recID,
