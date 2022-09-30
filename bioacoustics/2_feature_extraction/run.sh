2ccf2caf1341dfde88e5234feed9c066aab98d56
Jelle Treep <40466121+jelletreep@users.noreply.github.com>
2022-09-30 10:34:30 +0200

    Update run.sh

--- bioacoustics/2_feature_extraction/run.sh
+++ bioacoustics/2_feature_extraction/run.sh
@@ -3,5 +3,5 @@ output_dir="../../output/features/"
 
 echo "Processing $input_dir"
 
-python3 main.py --input_dir $input_dir --output_dir ${output_dir}chimpanze_24000.csv" --frame_length 24000 --hop_length 12000 --cores 8 --filter 100 2000 5 --label1 chimpanze --label2 test
+python3 extract_features_svm.py --input_dir $input_dir --output_dir ${output_dir}chimpanze_24000.csv" --frame_length 24000 --hop_length 12000 --cores 8 --filter 100 2000 5 --label1 chimpanze --label2 test
 
