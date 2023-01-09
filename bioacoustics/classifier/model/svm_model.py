0f04626ee67d02e70fac96b34d37dcf5639d0f95
Jelle Treep <h.j.treep@uu.nl>
2023-01-09 16:43:14 +0100

    add readthedocs.yaml

--- bioacoustics/classifier/model/svm_model.py
+++ bioacoustics/classifier/model/svm_model.py
@@ -1,13 +1,24 @@
+"""SVM model class."""
+
+import pickle
 from acoustic_model import AcousticModel
 from sklearn.model_selection import GridSearchCV
 from sklearn.svm import SVC
-import pickle
 
 
 class SVM_model(AcousticModel):
+    """SVM model.
+
+    Parameters
+    ----------
+    args : list
+        List of arguments.
+    """
+
     def __init__(self, *args):
-        super(SVM_model, self).__init__()
+        super().__init__()
         self.predicts = None
+        self.acoustic_model = None
 
     def _train(self, X_train, y_train, X_test, y_test, file_path):
         parameters = [
@@ -20,10 +31,11 @@ class SVM_model(AcousticModel):
         )
         self.acoustic_model.fit(X_train, y_train)
 
-        print("Best parameter (UAR=%0.3f):" % self.acoustic_model.best_score_)
+        print(f"Best parameter (UAR={self.acoustic_model.best_score_}")
         print(self.acoustic_model.best_params_)
         self._save_model(file_path)
 
     def _save_model(self, file_path):
         filename = file_path + "svm_model.sav"
-        pickle.dump(self.acoustic_model, open(filename, "wb"))
+        with open(filename, "wb") as file:
+            pickle.dump(self.acoustic_model, file)
