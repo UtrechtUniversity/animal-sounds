1be63d40588166f716b840f9a30d5cdc9c537431
Jelle Treep <h.j.treep@uu.nl>
2023-01-19 13:18:29 +0100

    fix sphinx

--- bioacoustics/classifier/hyperparameter_optimization/hpo_dropout_weight.py
+++ bioacoustics/classifier/hyperparameter_optimization/hpo_dropout_weight.py
@@ -62,61 +62,61 @@ def create_model(init_mode, dropout_rate, weight_constraint):
 
     return model
 
+if __name__ == "__main__":
+    parser = parse_arguments()
+    args = parser.parse_args()
 
-parser = parse_arguments()
-args = parser.parse_args()
+    if not os.path.exists(os.path.dirname(args.output_dir)):
+        os.makedirs(os.path.dirname(args.output_dir))
 
-if not os.path.exists(os.path.dirname(args.output_dir)):
-    os.makedirs(os.path.dirname(args.output_dir))
+    # fix random seed for reproducibility
+    seed = 7
+    tf.random.set_seed(seed)
 
-# fix random seed for reproducibility
-seed = 7
-tf.random.set_seed(seed)
+    X_train, y_train, X_test, y_test = prepare_data_dl(
+        args.feature_dir, norm_val_dir=args.normVal_dir, fraction=1
+    )
 
-X_train, y_train, X_test, y_test = prepare_data_dl(
-    args.feature_dir, norm_val_dir=args.normVal_dir, fraction=1
-)
+    # create model
+    callback = tf.keras.callbacks.EarlyStopping(monitor="loss", patience=3)
 
-# create model
-callback = tf.keras.callbacks.EarlyStopping(monitor="loss", patience=3)
+    model = KerasClassifier(
+        model=create_model,
+        loss="categorical_crossentropy",
+        optimizer="adam",
+        callbacks=[callback],
+        epochs=args.epochs,
+        batch_size=args.batch_size,
+        verbose=1,
+    )
 
-model = KerasClassifier(
-    model=create_model,
-    loss="categorical_crossentropy",
-    optimizer="adam",
-    callbacks=[callback],
-    epochs=args.epochs,
-    batch_size=args.batch_size,
-    verbose=1,
-)
+    # define the grid search parameters
+    init_mode = ["glorot_uniform"]  # ,"uniform","he_normal"
+    weight_constraint = [3.0]  # 1.0, 3.0, 5.0
+    dropout_rate = [0.2]  # ,0.5
+    param_grid = dict(
+        model__init_mode=init_mode,
+        model__dropout_rate=dropout_rate,
+        model__weight_constraint=weight_constraint,
+    )
 
-# define the grid search parameters
-init_mode = ["glorot_uniform"]  # ,"uniform","he_normal"
-weight_constraint = [3.0]  # 1.0, 3.0, 5.0
-dropout_rate = [0.2]  # ,0.5
-param_grid = dict(
-    model__init_mode=init_mode,
-    model__dropout_rate=dropout_rate,
-    model__weight_constraint=weight_constraint,
-)
+    grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=1, cv=3, refit=True)
+    grid_result = grid.fit(X_train, y_train)
 
-grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=1, cv=3, refit=True)
-grid_result = grid.fit(X_train, y_train)
+    # summarize results
+    print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+    means = grid_result.cv_results_["mean_test_score"]
+    stds = grid_result.cv_results_["std_test_score"]
+    params = grid_result.cv_results_["params"]
+    for mean, stdev, param in zip(means, stds, params):
+        print("%f (%f) with: %r" % (mean, stdev, param))
 
-# summarize results
-print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
-means = grid_result.cv_results_["mean_test_score"]
-stds = grid_result.cv_results_["std_test_score"]
-params = grid_result.cv_results_["params"]
-for mean, stdev, param in zip(means, stds, params):
-    print("%f (%f) with: %r" % (mean, stdev, param))
+    d = {"mean": means, "stdev": stds, "param": params}
+    pd.DataFrame(data=d).to_csv(args.output_dir + "_params.csv", index=False)
 
-d = {"mean": means, "stdev": stds, "param": params}
-pd.DataFrame(data=d).to_csv(args.output_dir + "_params.csv", index=False)
+    d = {"best_score": [grid_result.best_score_], "best_params": [grid_result.best_params_]}
+    pd.DataFrame(data=d).to_csv(args.output_dir + "_best_params.csv", index=False)
 
-d = {"best_score": [grid_result.best_score_], "best_params": [grid_result.best_params_]}
-pd.DataFrame(data=d).to_csv(args.output_dir + "_best_params.csv", index=False)
-
-# save model
-estimator = grid_result.best_estimator_
-dump(estimator, args.output_dir + "best_estimator.joblib")
+    # save model
+    estimator = grid_result.best_estimator_
+    dump(estimator, args.output_dir + "best_estimator.joblib")
--- bioacoustics/classifier/hyperparameter_optimization/hpo_epoch_batch.py
+++ bioacoustics/classifier/hyperparameter_optimization/hpo_epoch_batch.py
@@ -50,42 +50,42 @@ def create_model():
 
     return c_model
 
+if __name__ == "__main__":
+    parser = parse_arguments()
+    args = parser.parse_args()
 
-parser = parse_arguments()
-args = parser.parse_args()
+    if not os.path.exists(os.path.dirname(args.output_dir)):
+        os.makedirs(os.path.dirname(args.output_dir))
 
-if not os.path.exists(os.path.dirname(args.output_dir)):
-    os.makedirs(os.path.dirname(args.output_dir))
+    # fix random seed for reproducibility
+    seed = 7
+    tf.random.set_seed(seed)
 
-# fix random seed for reproducibility
-seed = 7
-tf.random.set_seed(seed)
+    X_train, y_train, X_test, y_test = prepare_data_dl(
+        args.feature_dir, norm_val_dir=args.normVal_dir, fraction=0.6
+    )
 
-X_train, y_train, X_test, y_test = prepare_data_dl(
-    args.feature_dir, norm_val_dir=args.normVal_dir, fraction=0.6
-)
+    # create model
+    callback = tf.keras.callbacks.EarlyStopping(monitor="loss", patience=3)
+    model = KerasClassifier(model=create_model, callbacks=[callback], verbose=1)
 
-# create model
-callback = tf.keras.callbacks.EarlyStopping(monitor="loss", patience=3)
-model = KerasClassifier(model=create_model, callbacks=[callback], verbose=1)
+    # define the grid search parameters
+    batch_size = [8, 32, 64]
+    epochs = [5, 10, 50]
+    param_grid = dict(batch_size=batch_size, epochs=epochs)
+    grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=1, cv=3)
+    grid_result = grid.fit(X_train, y_train)
 
-# define the grid search parameters
-batch_size = [8, 32, 64]
-epochs = [5, 10, 50]
-param_grid = dict(batch_size=batch_size, epochs=epochs)
-grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=1, cv=3)
-grid_result = grid.fit(X_train, y_train)
+    # summarize results
+    print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+    means = grid_result.cv_results_["mean_test_score"]
+    stds = grid_result.cv_results_["std_test_score"]
+    params = grid_result.cv_results_["params"]
+    for mean, stdev, param in zip(means, stds, params):
+        print("%f (%f) with: %r" % (mean, stdev, param))
 
-# summarize results
-print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
-means = grid_result.cv_results_["mean_test_score"]
-stds = grid_result.cv_results_["std_test_score"]
-params = grid_result.cv_results_["params"]
-for mean, stdev, param in zip(means, stds, params):
-    print("%f (%f) with: %r" % (mean, stdev, param))
+    d = {"mean": means, "stdev": stds, "param": params}
+    pd.DataFrame(data=d).to_csv(args.output_dir + "_params.csv", index=False)
 
-d = {"mean": means, "stdev": stds, "param": params}
-pd.DataFrame(data=d).to_csv(args.output_dir + "_params.csv", index=False)
-
-d = {"best_score": [grid_result.best_score_], "best_params": [grid_result.best_params_]}
-pd.DataFrame(data=d).to_csv(args.output_dir + "_best_params.csv", index=False)
+    d = {"best_score": [grid_result.best_score_], "best_params": [grid_result.best_params_]}
+    pd.DataFrame(data=d).to_csv(args.output_dir + "_best_params.csv", index=False)
